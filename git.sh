d2d607c881340e37600fa00c9e3a1e2a63d8aff1
shriman-dev <shrimankumbar@gmail.com>
2018-06-06 08:05:01 +0530

    first commit

---
+++ git.sh
@@ -0,0 +1,6 @@
+#echo "# diana-web-chat-client" >> README.md
+#git init
+#git add README.md
+git commit -m "first commit"
+#git remote add origin https://github.com/Evolvus/diana-web-chat-client.git
+git push -u origin master
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  dainaUrl:'https://powerful-stream-81254.herokuapp.com'
+};
