f48853cc090da2cdb6a5c4c5e7413e5089f02077
Shriman Nijagun <EVOLVUS\shrimank@ELPBLR-025.evolvus.com>
2018-06-07 18:42:56 +0530

    Sound added.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { IntentService } from '../../services/intent.service';
 import { Message } from '../../models/message';
 import { Subscription } from 'rxjs/Subscription';
+import { Howl } from 'howler';
 
 
 @Component({
@@ -16,13 +17,20 @@ export class DashboardComponent implements OnInit {
   messages: Message[];
   subscription: Subscription;
   @ViewChild('content') private myScrollContainer: ElementRef;
+  sound: any;
+  constructor(private intentService: IntentService) {
 
-  constructor(private intentService: IntentService) { }
+    this.sound = new Howl({
+      src: ['assets/tone.mp3'],
+      html5: true
+    });
+   }
 
   ngOnInit() {
     this.messages = [];
     this.subscription = this.intentService.intentsChanged.subscribe((msg: Message) => {
       this.messages.push(msg);
+      this.sound.play();
       this.scrollToBottom();
     });
   }
@@ -30,6 +38,7 @@ export class DashboardComponent implements OnInit {
   getQuery() {
     if (this.text.trim() !== '') {
       const msg = new Message(this.text.trim(), null, 'user');
+      this.sound.play();
       this.messages.push(msg);
       this.intentService.query(this.text);
       this.text = '';
